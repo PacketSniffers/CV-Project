ad4bede593a0667abb245d7f7a7944e1d3bb9148
kilozcode <kayodezollner@hotmail.com>
2018-11-27 15:09:06 +0000

    added test for login (yet to be implemented)

--- CV Backend/src/test/java/com/qa/demo/FrontEndTestSuite.java
+++ CV Backend/src/test/java/com/qa/demo/FrontEndTestSuite.java
@@ -5,7 +5,7 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ AccountCreationTester.class, LoginPageTester.class })
+@SuiteClasses({ AccountCreationTester.class, LoginPageTester.class , LoginTester.class})
 public class FrontEndTestSuite {
 
 }
